585d6d2cd78ff2b408f4e859fee01e025b0bc15f
Nicolas Andres Kurek <131825518+NinAndres@users.noreply.github.com>
2024-08-05 21:21:06 -0300

    Base folder

---
+++ src/main/java/com/carneseca/academiafaculdade/AcademiafaculdadeApplication.java
@@ -0,0 +1,13 @@
+package com.carneseca.academiafaculdade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiafaculdadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiafaculdadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carneseca/academiafaculdade/AcademiafaculdadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carneseca.academiafaculdade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiafaculdadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
